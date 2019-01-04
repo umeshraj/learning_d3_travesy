89c305ea3322cf20d981547dd3af28a632d124f5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-04 14:57:19 -0500

    Using data to change styles (font-size)

--- part_02_simple_data_binding/app.js
+++ part_02_simple_data_binding/app.js
@@ -1,7 +1,13 @@
 const numArray = [12, 20, 33, 40, 55];
 
+// // Changing the text of the li items
+// d3.selectAll(".items li")
+//   .data(numArray)
+//   .text(function(d) {
+//     return `This is item ${d}`;
+//   });
+
+// Changing the fontsize (style) of li items
 d3.selectAll(".items li")
   .data(numArray)
-  .text(function(d) {
-    return `This is item ${d}`;
-  });
+  .style("font-size", d => `${d}px`);
