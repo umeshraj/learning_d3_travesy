632508dfe4e6f4544a93e468d46ec15a2c13e73e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-04 19:24:34 -0500

    examples of data binding

--- part_02_simple_data_binding/app.js
+++ part_02_simple_data_binding/app.js
@@ -1,11 +1,12 @@
 const numArray = [12, 20, 33, 40, 55, 60, 75, 80, 90];
+const name = "John Doe";
 
-// // Changing the text of the li items
-// d3.selectAll(".items li")
-//   .data(numArray)
-//   .text(function(d) {
-//     return `This is item ${d}`;
-//   });
+// Changing the text of the li items
+d3.selectAll(".items li")
+  .data(numArray)
+  .text(function(d) {
+    return `This is item ${d}`;
+  });
 
 // // Changing the fontsize (style) of li items
 // d3.selectAll(".items li")
@@ -13,10 +14,31 @@ const numArray = [12, 20, 33, 40, 55, 60, 75, 80, 90];
 //   .style("font-size", d => `${d}px`);
 
 // Adding more nodes if you have more data than dom elements
-d3.selectAll(".items li")
+
+// // Note that this does not change the text in the first 5 li!!
+// d3.select("body")
+//   .selectAll(".items li")
+//   .data(numArray)
+//   .enter()
+//   .append("li")
+//   .text(d => `This is item ${d}`)
+//   .exit()
+//   .remove();
+
+// Recommended way of doing enter, exit
+const li = d3
+  .select("body")
+  .selectAll(".items li")
   .data(numArray)
-  .enter()
+  .text(d => `This is item number ${d}`);
+
+// handle extras
+li.enter()
   .append("li")
-  .text(d => `This is item ${d}`)
-  .exit()
-  .remove();
+  .text(d => `This is item number ${d}`);
+
+// exit
+li.exit().remove();
+
+// Changing some text
+d3.select(".name").text(name);
