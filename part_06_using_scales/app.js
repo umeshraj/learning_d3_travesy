feecea559bcaef6e005d721df5a79e6c93d31e73
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-06 19:59:17 -0500

    using scaleLinear() and scaleBand()

--- part_06_using_scales/app.js
+++ part_06_using_scales/app.js
@@ -4,8 +4,21 @@ const myData = [100, 10, 300, 400, 76, 24];
 // plotting constants
 const svgWidth = 400;
 const svgHeight = 400;
-const barWidth = 20;
-const barOffset = 10;
+
+// Set up the yscale
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(myData)])
+  .range([0, svgHeight]);
+
+// set up the x scale
+const xScale = d3
+  .scaleBand()
+  .domain(d3.range(0, myData.length))
+  .range([0, svgWidth]);
+// add some padding
+xScale.paddingInner(0.1);
+xScale.paddingOuter(0.1);
 
 // Plotting the chart
 const svg = d3
@@ -22,7 +35,7 @@ svg
   .enter()
   .append("rect")
   .style("fill", "lightgreen")
-  .attr("width", barWidth)
-  .attr("height", d => d)
-  .attr("x", (d, i) => i * (barWidth + barOffset))
-  .attr("y", d => svgHeight - d);
+  .attr("width", xScale.bandwidth())
+  .attr("height", d => yScale(d))
+  .attr("x", (d, i) => xScale(i))
+  .attr("y", d => svgHeight - yScale(d));
